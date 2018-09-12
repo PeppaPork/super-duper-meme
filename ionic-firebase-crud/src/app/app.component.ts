774266a57f0e244f809fa5295150d09efd3471b4
PeppaPork <42580366+PeppaPork@users.noreply.github.com>
2018-09-13 01:07:27 +0800

    config home page

--- ionic-firebase-crud/src/app/app.component.ts
+++ ionic-firebase-crud/src/app/app.component.ts
@@ -4,6 +4,17 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+// config firebase
+import * as firebase from 'firebase';
+const config = {
+  apiKey: 'AIzaSyAowRSpNvQV5zy5tTPeOMViVUl4KVgmBSQ',
+  authDomain: 'super-duper-meme.firebaseapp.com',
+  databaseURL: 'https://super-duper-meme.firebaseio.com',
+  projectId: 'super-duper-meme',
+  storageBucket: 'super-duper-meme.appspot.com',
+  messagingSenderId: '446656353588',
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -22,5 +33,7 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    // initialize firebase
+    firebase.initializeApp(config);
   }
 }
--- ionic-firebase-crud/src/app/home/home.page.ts
+++ ionic-firebase-crud/src/app/home/home.page.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
 
+// config for firebase et al
+import { AlertController } from '@ionic/angular';
+import { Router, ActivatedRoute } from '@angular/router';
+import * as firebase from 'Firebase';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+export class HomePage {
+  //  hold bulletin board list and referencing Firebase database
+  infos = [];
+  ref = firebase.database().ref('infos/');
+
+  //  listening any value changes from Firebase Database
+  constructor(public router: Router, public loadingController: LoadingController) {
+    this.ref.on('value', resp => {
+      this.infos = [];
+      this.infos = snapshotToArray(resp);
+    });
+  }
+
+  // add, edit and delete info
+  addInfo() {
+    this.router.navigate(['/add-info']);
+  }
+  edit(key) {
+    this.router.navigate(['/edit/' + key]);
+  }
+  async delete(key) {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Are you sure want to delete this info?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('cancel');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            firebase.database().ref('infos/' + key).remove();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
+
+export const snapshotToArray = snapshot => {
+  let returnArr = [];
+
+  snapshot.forEach(childSnapshot => {
+    let item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+  });
+
+  return returnArr;
+};
+
